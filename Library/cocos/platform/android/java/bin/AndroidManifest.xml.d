601222046c6530685ee4d28a011978e8ca42c3c2
samuele3hu <qingkui.hu@cocos2d-x.org>
2015-04-02 11:07:59 +0800

    Update cocos2d-x v3.5

--- Library/cocos/platform/android/java/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/cocos2d/MyWork/cocos2d-x-develop/cocos/platform/android/java/bin/AndroidManifest.xml : \
-/Users/cocos2d/MyWork/cocos2d-x-develop/cocos/platform/android/java/AndroidManifest.xml \
--- Library/cocos/platform/android/java/gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : /Users/cocos2d/MyWork/cocos2d-x-develop/cocos/platform/android/java/bin/AndroidManifest.xml \
